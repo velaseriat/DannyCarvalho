7b344adad0848b851f855faa507006e11ac38f15
aaflack18 <kmasuda@mines.edu>
2015-07-26 23:49:14 -1000

    added disqus forums to site

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -4,12 +4,12 @@ class SubscriberMailer < ActionMailer::Base
   def new_event(subscriber, event)
   	@subscriber = subscriber
   	@event = event
-    mail to: subscriber.email, subject: "Aloha!"
+    mail to: subscriber.email, subject: "Danny Carvalho Newsletter - Upcoming Event"
   end
 
   def send_custom_email(subscriber, text)
     @subscriber = subscriber
   	@text = text
-  	mail to: subscriber.email, subject: "THE TEST TEXT!"
+  	mail to: subscriber.email, subject: "Danny Carvalho Newsletter"
   end
 end
