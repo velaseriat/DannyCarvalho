8d29479021f6980da5693acccc03ab588493010a
Kelly Masuda <velaseriat@outlook.com>
2015-10-10 20:40:24 -1000

    Fixed some bugs regarding missing location in events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927044033) do
+ActiveRecord::Schema.define(version: 20151011063308) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name",           limit: 255
